ef8a319e490190620152a8fadbe24f1075d99b3e
danutz87 <danielghimpu@yahoo.com>
2020-05-07 16:12:37 +0100

    Basic dices game

---
+++ index.js
@@ -0,0 +1,23 @@
+
+let randomNumber1 = Math.ceil(Math.random() * 6);
+let randomNumber2 = Math.ceil(Math.random() * 6);
+
+let randomImage1 = "images/dice" + randomNumber1 + ".png"
+let randomImage2 = "images/dice" + randomNumber2 + ".png"
+
+let image1 = document.querySelectorAll("img")[0]
+  image1.setAttribute("src", randomImage1);
+
+let image2 = document.querySelectorAll("img")[1]
+  image2.setAttribute("src", randomImage2);
+
+
+if(randomNumber1 > randomNumber2){
+  document.querySelector("h1").innerHTML = "🚩 Player 1 wins!";
+}
+else if(randomNumber1 < randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 2 wins! 🚩";
+}
+else {
+  document.querySelector("h1").innerHTML = "It's a tie,try again!";
+}
